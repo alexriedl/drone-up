034776b68db72ee47717d81917d82c54b0c2268b
Phillip Forsythe <PForsythe@directsupply.com>
2017-08-21 15:23:15 -0500

    Adding temporary endpoints.

--- DroneUp/Controllers/HomeController.cs
+++ DroneUp/Controllers/HomeController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DroneUp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DroneUp.Controllers
 {
     public class HomeController : Controller
     {
+	    private readonly SetupService setupService;
+
+	    public HomeController()
+	    {
+		    setupService = new SetupService();
+	    }
+
         public IActionResult Index()
         {
             return View();
@@ -31,5 +39,35 @@ namespace DroneUp.Controllers
         {
             return View();
         }
-    }
+
+
+	    [HttpPost("/api/upload")]
+	    public IActionResult UploadDroneScript(string droneName, string droneScriptContent)
+	    {
+		    if (setupService.UploadDroneScript(droneName, droneScriptContent))
+		    {
+			    return Ok();
+		    }
+		    return Error();
+		}
+
+	    [HttpGet("/api/mapseed")]
+	    public IActionResult GetMapSeed()
+	    {
+		    return Ok(setupService.GetMapSeed());
+		}
+
+	    [HttpGet("/api/dronenames")]
+	    public IActionResult GetDroneScriptNames()
+	    {
+		    return Ok(setupService.GetDroneScriptNames());
+		}
+
+	    [HttpGet("/api/dronescripts")]
+	    public IActionResult GetDroneScripts(List<string> droneScriptNames)
+	    {
+		    return Ok(setupService.GetDroneScripts(droneScriptNames));
+
+	    }
+	}
 }
