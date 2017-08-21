05e3cd1abbac813519b21b91fda65c952a9457b2
Alex Riedl <alexanderbriedl@gmail.com>
2017-08-21 17:32:20 -0500

    Renderer work

--- DroneUp/scripts/renderer.ts
+++ DroneUp/scripts/renderer.ts
@@ -1,3 +1,8 @@
+import '@diamondlovesyou/tsm';
+
+export enum RenderObjectTypes {
+  Rectangle,
+}
 export interface ICoords {
   x: number;
   y: number;
@@ -8,15 +13,13 @@ export interface IColor {
   b: number;
 }
 export interface IRenderObject {
+  type: RenderObjectTypes;
   origin: ICoords;
 }
-export class Rectangle implements IRenderObject {
-  public constructor(
-    public width: number,
-    public height: number,
-    public origin: ICoords = { x: 0, y: 0 },
-    public color: IColor = { r: 1, g: 1, b: 1 }
-  ) { }
+export interface Rectangle extends IRenderObject {
+  width: number;
+  height: number;
+  color: IColor;
 }
 
 function InitWebGL(canvas) {
@@ -47,6 +50,11 @@ function CreateShaderProgram(gl: WebGLRenderingContext, vertexShaderSource: stri
   var vertexShader = CreateShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
   var fragmentShader = CreateShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 
+  if(!vertexShader || !fragmentShader) {
+    console.log("Failed to compile one of the shaders...");
+    return;
+  }
+
   var program = gl.createProgram();
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
@@ -66,54 +74,88 @@ export class Renderer {
   private program: WebGLShader;
 
   private positionAttributeLocation: number;
+  private positionAttributeColor: number;
+
+  private rectangleVertexBuffer: WebGLBuffer;
+  private rectangleColorBuffer: WebGLBuffer;
 
   private vertexShaderSource = `
   attribute vec4 a_position;
+  attribute vec4 a_color;
+
+  varying lowp vec4 v_color;
 
   void main() {
     gl_Position = a_position;
+    v_color = a_color;
   }
   `;
   private fragmentShaderSource = `
   precision mediump float;
+  varying lowp vec4 v_color;
 
   void main() {
-    gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+    gl_FragColor = v_color;
+    // gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
   }
   `;
 
   public Initialize(canvasId: string) {
-    let canvas = document.getElementById(canvasId);
+    this.canvas = document.getElementById(canvasId);
 
-    this.gl = InitWebGL(canvas);
-    if (!this.gl) return;
+    const gl = InitWebGL(this.canvas);
+    if (!gl) return;
 
-    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    this.gl.enable(this.gl.DEPTH_TEST);
-    this.gl.depthFunc(this.gl.LEQUAL);
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    this.gl = gl;
 
-    this.program = CreateShaderProgram(this.gl, this.vertexShaderSource, this.fragmentShaderSource);
-    this.positionAttributeLocation = this.gl.getAttribLocation(this.program, "a_position");
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    this.program = CreateShaderProgram(gl, this.vertexShaderSource, this.fragmentShaderSource);
+    this.positionAttributeLocation = gl.getAttribLocation(this.program, "a_position");
+    this.positionAttributeColor = gl.getAttribLocation(this.program, "a_color");
+
+    gl.enableVertexAttribArray(this.positionAttributeLocation);
+    gl.enableVertexAttribArray(this.positionAttributeColor);
+
+    this.InitializeRectangleBuffers(gl);
+  }
+
+  public InitializeRectangleBuffers(gl: WebGLRenderingContext) {
+    var positions = [
+      -0.5, -0.5,
+      0.5, -0.5,
+      0.5, 0.5,
+      -0.5, 0.5,
+    ];
+    this.rectangleVertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.rectangleVertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    var colors = [
+      1, 1, 1, 1,
+      1, 1, 1, 1,
+      1, 1, 1, 1,
+      1, 1, 1, 1,
+      // object.color.r, object.color.b, object.color.g, 1,
+      // object.color.r, object.color.b, object.color.g, 1,
+      // object.color.r, object.color.b, object.color.g, 1,
+      // object.color.r, object.color.b, object.color.g, 1,
+    ];
+    this.rectangleColorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.rectangleColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    this.rectangleColorBuffer = gl.createBuffer();
   }
 
   /*************************************************************************
   *************************************************************************/
 
   private RenderRectangle(gl: WebGLRenderingContext, object: Rectangle) {
-    var positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    var positions = [
-      0, 0,
-      0, 0.5,
-      0.7, 0,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(this.positionAttributeLocation);
-
-    // Bind the position buffer.
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.rectangleVertexBuffer);
     // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
     var size = 2;          // 2 components per iteration
     var type = gl.FLOAT;   // the data is 32bit floats
@@ -122,9 +164,19 @@ export class Renderer {
     var offset = 0;        // start at the beginning of the buffer
     gl.vertexAttribPointer(this.positionAttributeLocation, size, type, normalize, stride, offset)
 
-    var primitiveType = gl.TRIANGLES;
+    var colors = [
+      object.color.r, object.color.b, object.color.g, 1,
+      object.color.r, object.color.b, object.color.g, 1,
+      object.color.r, object.color.b, object.color.g, 1,
+      object.color.r, object.color.b, object.color.g, 1,
+    ];
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.rectangleColorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(this.positionAttributeColor, 4, gl.FLOAT, false, 0, 0);
+
+    var primitiveType = gl.TRIANGLE_FAN;
     var offset = 0;
-    var count = 3;
+    var count = 4;
     gl.drawArrays(primitiveType, offset, count);
   }
 
@@ -138,30 +190,41 @@ export class Renderer {
     this.gl.viewport(0, 0, this.canvas['width'], this.canvas['height']);
     gl.useProgram(this.program);
 
-    group.objects.forEach(object => {
-      switch (typeof object) {
-        case typeof Rectangle:
-          this.RenderRectangle(gl, <Rectangle>object);
+    group.objects.forEach(o => {
+      switch ((<IRenderObject>o).type) {
+        case RenderObjectTypes.Rectangle:
+          this.RenderRectangle(gl, <Rectangle>o);
           break;
+        default:
+          console.log("UNKNOWN OBJECT");
       }
     });
   }
 }
 
 export class RenderGroup {
-  public objects: IRenderObject[] = [];
+  public objects: Object[] = [];
 
-  public PushRectangle(object: Rectangle) {
-    this.objects.push(object);
+  public PushRectangle(origin: ICoords, width: number, height: number) {
+    this.objects.push({
+      type: RenderObjectTypes.Rectangle,
+      origin: origin,
+      width: width,
+      height: height,
+      color: { r: 0.5, g: 1, b: 1 },
+    });
   }
 }
 
 export function test() {
+  console.log("Setting up");
   const renderer = new Renderer();
   renderer.Initialize("game-canvas");
 
+  console.log("Building frame");
   const frame = new RenderGroup();
-  frame.PushRectangle(new Rectangle(10, 10));
+  frame.PushRectangle({x: 0, y: 0}, 10, 10);
 
+  console.log("Rendering");
   renderer.RenderOutput(frame);
 }
