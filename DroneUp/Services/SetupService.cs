c8cef7b810983e29f7f844fb34f8f0c719503dd5
Phillip Forsythe <PForsythe@directsupply.com>
2017-08-21 14:44:12 -0500

    Adding basic setup service for server side.

---
+++ DroneUp/Services/SetupService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DroneUp.Services
+{
+    public class SetupService
+    {
+	    private const string DRONES_DIRECTORY = "./drones";
+
+		public int GetMapSeed()
+	    {
+			Random random = new Random();
+		    return random.Next(1, int.MaxValue);
+		}
+
+	    public bool UploadScript(string droneScriptName, string droneScriptContent)
+	    {
+		    bool successfullyUploadedScript = false;
+		    string writePath = DRONES_DIRECTORY + "/" + droneScriptName;
+
+		    if (!File.Exists(writePath))
+		    {
+				File.WriteAllText(writePath, droneScriptContent);
+
+			    successfullyUploadedScript = File.Exists(writePath);
+		    }
+
+			return successfullyUploadedScript;
+	    }
+
+	    public List<string> GetDroneScriptNames()
+	    {
+		    List<string> allDroneScriptNames = new List<string>();
+		    var files = new DirectoryInfo(DRONES_DIRECTORY).GetFiles();
+
+		    foreach (var file in files)
+		    {
+			    allDroneScriptNames.Add(file.Name);
+		    }
+			
+			return allDroneScriptNames;
+	    }
+
+	    public Dictionary<string, string> GetDroneScripts(List<string> droneScriptNames)
+	    {
+		    Dictionary<string, string> droneScripts = new Dictionary<string, string>();
+
+		    foreach (var droneScriptName in droneScriptNames)
+		    {
+				var readPath = DRONES_DIRECTORY + "/" + droneScriptName;
+			    var droneScriptContent = File.ReadAllText(readPath);
+
+				droneScripts.Add(droneScriptName, droneScriptContent);
+		    }
+
+		    return droneScripts;
+	    }
+    }
+}
