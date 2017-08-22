bfafef013bd6784dd80c4aea3a5ffccfa6ddf963
Phillip Forsythe <PForsythe@directsupply.com>
2017-08-22 13:31:25 -0500

    Philmit

--- DroneUp/Controllers/HomeController.cs
+++ DroneUp/Controllers/HomeController.cs
@@ -7,18 +7,47 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DroneUp.Controllers
 {
-  public class HomeController : Controller
-  {
-    private readonly SetupService setupService;
+	public class HomeController : Controller
+	{
+		private readonly SetupService setupService;
 
-    public HomeController()
-    {
-      setupService = new SetupService();
-    }
+		public HomeController()
+		{
+			setupService = new SetupService();
+		}
 
-    public IActionResult Index()
-    {
-      return View();
-    }
-  }
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost("/api/upload")]
+		public IActionResult UploadDroneScript(string droneName, string droneScriptContent)
+		{
+			if (setupService.UploadDroneScript(droneName, droneScriptContent))
+			{
+				return Ok();
+			}
+			throw new Exception();
+		}
+
+		[HttpGet("/api/mapseed")]
+		public IActionResult GetMapSeed()
+		{
+			return Ok(setupService.GetMapSeed());
+		}
+
+		[HttpGet("/api/dronenames")]
+		public IActionResult GetDroneScriptNames()
+		{
+			return Ok(setupService.GetDroneScriptNames());
+		}
+
+		[HttpGet("/api/dronescripts")]
+		public IActionResult GetDroneScripts(List<string> droneScriptNames)
+		{
+			return Ok(setupService.GetDroneScripts(droneScriptNames));
+
+		}
+	}
 }
