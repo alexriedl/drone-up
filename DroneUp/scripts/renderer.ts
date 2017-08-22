fb81626cebf727ce05727dfe4809bfc18966c681
Alex Riedl <alexanderbriedl@gmail.com>
2017-08-22 11:30:57 -0500

    renderer examples

--- DroneUp/scripts/renderer.ts
+++ DroneUp/scripts/renderer.ts
@@ -23,6 +23,7 @@ export interface IRenderContext {
 }
 
 export class Renderer {
+  private tileSize: number = 1;
   private context: IRenderContext;
   private xScale: number;
   private yScale: number;
@@ -51,25 +52,44 @@ export class Renderer {
   public renderState(state: IMapState) {
     this.xScale = this.context.width / state.xSize;
     this.yScale = this.context.height / state.ySize;
+
+    this.context.context.clearRect(0, 0, this.context.width, this.context.height);
+
     this.renderGrid(state.xSize, state.ySize);
-    state.mapObjects.forEach(object => {
-      //this.renderObject();
+    state.mapObjects.forEach(entity => {
+      this.renderObject(entity);
     });
   }
 
-  private
+  private getPlayerColor(id: number): string {
+    switch(id) {
+      case 0: return "pink";
+      case 1: return "green";
+      case 2: return "blue";
+      case 3: return "yellow";
+      case 4: return "red";
+    }
+  }
+
+  private renderObject(entity: IEntity) {
+    const isPlayer = entity.ID.startsWith("player");
+    const id = isPlayer && entity.ID.substr(6);
+    const color = isPlayer ? this.getPlayerColor(+id) : "gray";
+
+    this.rectangle(entity.x, entity.y, this.tileSize, this.tileSize, color);
+  }
 
   private renderGrid(width: number, height: number) {
-    const step = 1;
+    const step = this.tileSize;
 
     for (var x = 0; x <= width; x += step) {
       this.moveTo(x, 0);
-      this.lineTo(x, width);
+      this.lineTo(x, height);
     }
 
-    for (var x = 0; x <= height; x += step) {
-      this.moveTo(0, x);
-      this.lineTo(height, x);
+    for (var y = 0; y <= height; y += step) {
+      this.moveTo(0, y);
+      this.lineTo(width, y);
     }
 
     this.stroke();
@@ -83,6 +103,14 @@ export class Renderer {
     this.context.context.lineTo(x * this.xScale, y * this.yScale * this.context.aspect);
   }
 
+  private rectangle(x: number, y: number, width: number, height: number, color: string) {
+    const context = this.context.context;
+    const aspect = this.context.aspect;
+
+    context.fillStyle = color;
+    context.fillRect(x * this.xScale, y * this.yScale * aspect, width * this.xScale, height * this.yScale * aspect);
+  }
+
   private stroke(style: string = "black") {
     this.context.context.strokeStyle = style;
     this.context.context.stroke();
@@ -92,12 +120,34 @@ export class Renderer {
 
 export function test() {
   const renderer = new Renderer("game-canvas");
+
   renderer.renderState({
     invalidArray: [],
-    mapObjects: [],
+    mapObjects: [
+      { ID: "player1", x: 7, y: 1 },
+      { ID: "player2", x: 3, y: 1 },
+      { ID: "player3", x: 15, y: 6 },
+      { ID: "__reservedSpikeNumber1__", x: 3, y: 4 },
+    ],
     players: [],
     spikes: [],
-    xSize: 30,
-    ySize: 30
+    xSize: 50,
+    ySize: 50
   });
+
+  setTimeout(function() {
+    renderer.renderState({
+      invalidArray: [],
+      mapObjects: [
+        { ID: "player1", x: 10, y: 1 },
+        { ID: "player2", x: 15, y: 1 },
+        { ID: "player3", x: 15, y: 7 },
+        { ID: "__reservedSpikeNumber1__", x: 3, y: 4 },
+      ],
+      players: [],
+      spikes: [],
+      xSize: 50,
+      ySize: 50
+    });
+  }, 3000);
 }
