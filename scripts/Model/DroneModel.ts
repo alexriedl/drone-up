d01705670273b0f0acb6ef50d18663fca806f6f6
Alex Riedl <alexanderbriedl@gmail.com>
2017-09-21 08:02:30 -0500

    delete unused drone model

--- scripts/Model/DroneModel.ts
+++
@@ -1,8 +0,0 @@
-import { Color } from '../Utils';
-import SimpleRectangle from './SimpleRectangle';
-
-export default class DroneModel extends SimpleRectangle {
-	public constructor(color: Color) {
-		super(color);
-	}
-}
