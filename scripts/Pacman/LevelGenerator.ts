7350d8bc5051faae12ef1de9f301c75bc2555084
Alex Riedl <alexanderbriedl@gmail.com>
2017-09-25 06:55:43 -0500

    start setting up movement for pacman

--- scripts/Pacman/LevelGenerator.ts
+++ scripts/Pacman/LevelGenerator.ts
@@ -1,9 +1,9 @@
 // tslint:disable:no-bitwise
 
 import { Color } from 'Engine/Utils';
-import { SimpleTextureRectangle } from 'Engine/Model';
-import { vec3 } from 'Engine/Math';
-import Entity from 'Engine/Entity';
+import { Entity } from 'Engine/Entity';
+import { SimpleTextureRectangle, SimpleRectangle } from 'Engine/Model';
+import { vec2, vec3 } from 'Engine/Math';
 import Renderer from 'Engine/Renderer';
 
 enum MI {
@@ -50,24 +50,38 @@ enum MI {
 	WNW, // West (barrior) North West
 	WSW, // West (barrior) South West
 
+	// Starting Points
+	_PS, // Pac Start
+	_FS, // Fruit Start
+	GSB, // Ghost Start Blinky
+	GSP, // Ghost Start Pinky
+	GSI, // Ghost Start Inky
+	GSC, // Ghost Start Clyde
+
+	// Pacs
 	_p_, // Pac
 	_E_, // Energizer
+
+	// Special
+	_s_, // Slow (Empty)
+	RU_, // Restrict Up
+	RR_, // Restrict Right
+	RUp, // Restrict Up (with a pac)
+	GTB, // Ghost Target Blinky
+	GTP, // Ghost Target Pinky
+	GTI, // Ghost Target Inky
+	GTC, // Ghost Target Clyde
+
+	// Extra
 	FFF, // Full
 	___, // Empty
 }
 
-const smallDemo = [
-	[MI.DNW, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DNE],
-	[MI.DW_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.DE_],
-	[MI.DW_, MI.___, MI.SSE, MI.SS_, MI.SSW, MI.___, MI.DE_],
-	[MI.DW_, MI.___, MI.SE_, MI.___, MI.SW_, MI.___, MI.DE_],
-	[MI.DW_, MI.___, MI.SNE, MI.SN_, MI.SNW, MI.___, MI.DE_],
-	[MI.DW_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.DE_],
-	[MI.DSW, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DSE],
-];
-
 // tslint:disable:max-line-length
 const original = [
+	[MI.___, MI.___, MI.GTP, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.GTB, MI.___, MI.___],
+	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___],
+	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___],
 	[MI.DNW, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.NNE, MI.NNW, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DNE],
 	[MI.DW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.DE_],
 	[MI.DW_, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.SE_, MI.SW_, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.DE_],
@@ -79,19 +93,19 @@ const original = [
 	[MI.DW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.DE_],
 	[MI.DSW, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.SSW, MI._p_, MI.SE_, MI.ISW, MI.SS_, MI.SS_, MI.SSW, MI.___, MI.SE_, MI.SW_, MI.___, MI.SSE, MI.SS_, MI.SS_, MI.ISE, MI.SW_, MI._p_, MI.SSE, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DSE],
 	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.INW, MI.SN_, MI.SN_, MI.SNW, MI.___, MI.SNE, MI.SNW, MI.___, MI.SNE, MI.SN_, MI.SN_, MI.INE, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
-	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.SW_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.SE_, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
+	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.SW_, MI.___, MI.___, MI.___, MI.RU_, MI.GSB, MI.RR_, MI.RU_, MI.___, MI.___, MI.___, MI.SE_, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
 	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.SW_, MI.___, MI.PSE, MI.DS_, MI.PEW, MI.GGG, MI.GGG, MI.PEE, MI.DS_, MI.PSW, MI.___, MI.SE_, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
 	[MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.SNW, MI._p_, MI.SNE, MI.SNW, MI.___, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI.___, MI.SNE, MI.SNW, MI._p_, MI.SNE, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_],
-	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI._p_, MI.___, MI.___, MI.___, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI.___, MI.___, MI.___, MI._p_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___],
+	[MI._s_, MI._s_, MI._s_, MI._s_, MI._s_, MI.___, MI._p_, MI.___, MI.___, MI.___, MI.DE_, MI.GSI, MI.___, MI.GSP, MI.___, MI.GSC, MI.___, MI.DW_, MI.___, MI.___, MI.___, MI._p_, MI.___, MI._s_, MI._s_, MI._s_, MI._s_, MI._s_],
 	[MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.SSW, MI._p_, MI.SSE, MI.SSW, MI.___, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI.___, MI.SSE, MI.SSW, MI._p_, MI.SSE, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_],
 	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.SW_, MI.___, MI.PNE, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.PNW, MI.___, MI.SE_, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
-	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.SW_, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.SE_, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
+	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.SW_, MI.___, MI.___, MI.___, MI.___, MI._FS, MI.___, MI.___, MI.___, MI.___, MI.___, MI.SE_, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
 	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.DW_, MI._p_, MI.SE_, MI.SW_, MI.___, MI.SSE, MI.SS_, MI.SS_, MI.SS_, MI.SS_, MI.SS_, MI.SS_, MI.SSW, MI.___, MI.SE_, MI.SW_, MI._p_, MI.DE_, MI.___, MI.___, MI.___, MI.___, MI.___],
 	[MI.DNW, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.SNW, MI._p_, MI.SNE, MI.SNW, MI.___, MI.SNE, MI.SN_, MI.SN_, MI.INE, MI.INW, MI.SN_, MI.SN_, MI.SNW, MI.___, MI.SNE, MI.SNW, MI._p_, MI.SNE, MI.DN_, MI.DN_, MI.DN_, MI.DN_, MI.DNE],
 	[MI.DW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.DE_],
 	[MI.DW_, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.SE_, MI.SW_, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.DE_],
 	[MI.DW_, MI._p_, MI.SNE, MI.SN_, MI.INE, MI.SW_, MI._p_, MI.SNE, MI.SN_, MI.SN_, MI.SN_, MI.SNW, MI._p_, MI.SNE, MI.SNW, MI._p_, MI.SNE, MI.SN_, MI.SN_, MI.SN_, MI.SNW, MI._p_, MI.SE_, MI.INW, MI.SN_, MI.SNW, MI._p_, MI.DE_],
-	[MI.DW_, MI._E_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.___, MI.___, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._E_, MI.DE_],
+	[MI.DW_, MI._E_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.RUp, MI._PS, MI.___, MI.RUp, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._E_, MI.DE_],
 	[MI.WSW, MI.SS_, MI.SSW, MI._p_, MI.SE_, MI.SW_, MI._p_, MI.SSE, MI.SSW, MI._p_, MI.SSE, MI.SS_, MI.SS_, MI.SS_, MI.SS_, MI.SS_, MI.SS_, MI.SSW, MI._p_, MI.SSE, MI.SSW, MI._p_, MI.SE_, MI.SW_, MI._p_, MI.SSE, MI.SS_, MI.ESE],
 	[MI.WNW, MI.SN_, MI.SNW, MI._p_, MI.SNE, MI.SNW, MI._p_, MI.SE_, MI.SW_, MI._p_, MI.SNE, MI.SN_, MI.SN_, MI.INE, MI.INW, MI.SN_, MI.SN_, MI.SNW, MI._p_, MI.SE_, MI.SW_, MI._p_, MI.SNE, MI.SNW, MI._p_, MI.SNE, MI.SN_, MI.ENE],
 	[MI.DW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI.SE_, MI.SW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.DE_],
@@ -99,19 +113,15 @@ const original = [
 	[MI.DW_, MI._p_, MI.SNE, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SNW, MI._p_, MI.SNE, MI.SNW, MI._p_, MI.SNE, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SN_, MI.SNW, MI._p_, MI.DE_],
 	[MI.DW_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI._p_, MI.DE_],
 	[MI.DSW, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DS_, MI.DSE],
+	[MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___],
+	[MI.GTC, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.___, MI.GTI],
 ];
 
 const tiles = original;
 
-const BORDER = [0x21, 0x21, 0xFF, 0xFF];
-const PAC = [0xFF, 0xFF, 0xFF, 0xFF];
-const GATE = [0xFF, 0xB8, 0xFF, 0xFF];
-const EMPTY = [0x00, 0x00, 0x00, 0x00];
-
-const boardWidth = tiles[0].length;
-const boardHeight = tiles.length;
-const TILE_WIDTH = 8;
-const TILE_HEIGHT = 8;
+const NUM_X_TILES = tiles[0].length;
+const NUM_Y_TILES = tiles.length;
+const PIXELS_PER_TILE = 8;
 
 function isBitSet(bit: number, value: number): boolean {
 	return !!(value & (1 << bit));
@@ -160,38 +170,61 @@ function getTileValue(tile: MI): number[] {
 		case MI.WNW: return [0x80, 0x80, 0x80, 0x87, 0x88, 0x90, 0x90, 0x90];
 		case MI.WSW: return [0x90, 0x90, 0x90, 0x88, 0x87, 0x80, 0x80, 0x80];
 
+		case MI.RUp:
 		case MI._p_: return [0x00, 0x00, 0x00, 0x18, 0x18, 0x00, 0x00, 0x00];
 		case MI._E_: return [0x3C, 0x7E, 0xFF, 0xFF, 0xFF, 0xFF, 0x7E, 0x3C];
 
+		case MI._PS:
+		case MI.GSB:
+		case MI.GSP:
+		case MI.GSI:
+		case MI.GSC:
+			return [0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00];
+
+		case MI.GTB: case MI.GTP: case MI.GTI: case MI.GTC:
 		case MI.FFF: return [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF];
 		case MI.___: default: return [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 	}
 }
 
+const COLOR_BORDER = new Color(0x21, 0x21, 0xFF, 0xFF).rgba;
+const COLOR_GATE = new Color(0xFF, 0xB8, 0xFF, 0xFF).rgba;
+const COLOR_EMPTY = Color.BLACK.scale(255).rgba;
+const COLOR_PAC = new Color(0xFF, 0xDE, 0xD2, 0xFF).rgba;
+const COLOR_PACMAN = new Color(0xFF, 0xCC, 0x00, 0xFF).rgba;
+const COLOR_BLINKY = new Color(0xFF, 0x00, 0x00, 0xFF).rgba;
+const COLOR_PINKY = new Color(0xFF, 0xB8, 0xFF, 0xFF).rgba;
+const COLOR_INKY = new Color(0x00, 0xFF, 0xFF, 0xFF).rgba;
+const COLOR_CLYDE = new Color(0xFF, 0xB8, 0x51, 0xFF).rgba;
 function getTileColor(tile: MI): number[] {
 	switch (tile) {
-		case MI.___: return EMPTY;
-		case MI._p_: case MI._E_: return PAC;
-		case MI.GGG: return GATE;
-		default: return BORDER;
+		case MI.___: return COLOR_EMPTY;
+		case MI.RUp: case MI._p_: case MI._E_: return COLOR_PAC;
+		case MI.GGG: return COLOR_GATE;
+		case MI._PS: return COLOR_PACMAN;
+		case MI.GSB: case MI.GTB: return COLOR_BLINKY;
+		case MI.GSP: case MI.GTP: return COLOR_PINKY;
+		case MI.GSI: case MI.GTI: return COLOR_INKY;
+		case MI.GSC: case MI.GTC: return COLOR_CLYDE;
+		default: return COLOR_BORDER;
 	}
 }
 
 function generateLevelTextureData(): Uint8Array {
 	const data = [];
-	for (let tileY = 0; tileY < boardHeight; tileY++) {
+	for (let tileY = 0; tileY < NUM_Y_TILES; tileY++) {
 		// if (tileY < 3 || tileY > 33) continue;
 		const tileRow = tiles[tileY];
-		for (let pixelY = 0; pixelY < TILE_HEIGHT; pixelY++) {
-			for (let tileX = 0; tileX < boardWidth; tileX++) {
+		for (let pixelY = 0; pixelY < PIXELS_PER_TILE; pixelY++) {
+			for (let tileX = 0; tileX < NUM_X_TILES; tileX++) {
 				const tileEnum = tileRow[tileX];
 				const tile = getTileValue(tileEnum);
 				const color = getTileColor(tileEnum);
 				const pixelRow = tile[pixelY];
 
-				for (let pixelX = TILE_WIDTH - 1; pixelX >= 0; pixelX--) {
+				for (let pixelX = PIXELS_PER_TILE - 1; pixelX >= 0; pixelX--) {
 					const pixel = isBitSet(pixelX, pixelRow);
-					data.push.apply(data, pixel ? color : EMPTY);
+					data.push.apply(data, pixel ? color : COLOR_EMPTY);
 				}
 			}
 		}
@@ -200,12 +233,11 @@ function generateLevelTextureData(): Uint8Array {
 	return new Uint8Array(data);
 }
 
-function generateLevelTexture(gl: WebGLRenderingContext): WebGLTexture {
+function generateLevelTexture(gl: WebGLRenderingContext, width: number, height: number): WebGLTexture {
 	const texture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, texture);
 	gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
-		boardWidth * TILE_WIDTH, boardHeight * TILE_HEIGHT, 0,
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0,
 		gl.RGBA, gl.UNSIGNED_BYTE, generateLevelTextureData());
 
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
@@ -217,22 +249,27 @@ function generateLevelTexture(gl: WebGLRenderingContext): WebGLTexture {
 	return texture;
 }
 
-function createLevel(gl: WebGLRenderingContext): Entity {
-	const texture = generateLevelTexture(gl);
+function createLevel(gl: WebGLRenderingContext, width: number, height: number): Entity {
+	const texture = generateLevelTexture(gl, width, height);
 
 	const levelModel = new SimpleTextureRectangle(texture);
-	const levelObject = new Entity(levelModel,
-		new vec3(boardWidth / 2, boardHeight / 2),
-		new vec3(boardWidth, boardHeight, 1));
-
-	return levelObject;
+	return new Entity(levelModel,
+		new vec3(width / 2, height / 2),
+		new vec3(width, height, 1));
 }
 
 function start(): void {
-	const renderer = new Renderer('game-canvas', boardWidth, boardHeight);
-	const level = createLevel(renderer.gl);
+	const width = NUM_X_TILES * PIXELS_PER_TILE;
+	const height = NUM_Y_TILES * PIXELS_PER_TILE;
+	const renderer = new Renderer('game-canvas', width, height);
 	const background = Color.BLACK;
 	let then;
+
+	const level = createLevel(renderer.gl, width, height);
+	const test = new PacEntity(new vec2(1, 4));
+	const scene = new Entity();
+	level.setParent(scene);
+	test.setParent(scene);
 
 	const frame = (now: number) => {
 		const deltaTime = now - then;
@@ -246,12 +283,118 @@ function start(): void {
 			}
 		}
 
-		renderer.simpleRender(level, background);
+		scene.update(deltaTime);
+		renderer.simpleRender(scene, background);
 
 		requestAnimationFrame(frame);
 	};
 
 	requestAnimationFrame(frame);
+}
+
+class PacEntity extends Entity {
+	private facing: vec2;
+
+	public position: vec3;
+	public scale: vec3;
+
+	public constructor(startTile: vec2, size: vec2 = new vec2(16, 16)) {
+		super(new SimpleRectangle(Color.YELLOW));
+		this.position = new vec3((startTile.x + 0.5) * PIXELS_PER_TILE, (startTile.y + 0.5) * PIXELS_PER_TILE);
+		this.scale = size.toVec3(1);
+		this.facing = new vec2(0, 1);
+	}
+
+	public get currentTile(): vec2 {
+		return new vec2();
+	}
+
+	public get currentPixel(): vec2 {
+		return new vec2();
+	}
+
+	/**
+	 * Get which tile this entity is currently on. Tile (0, 0) is the upper left most tile
+	 */
+	public getMapCoords(): vec2 {
+		return PacEntity.getMapCoords(vec2.fromArray(this.position.xy));
+	}
+
+	/**
+	 * Get which pixel within a tile this entity is currently on. Pixel (0, 0) is the upper left most pixel within a tile
+	 */
+	public getTileCoords(): vec2 {
+		return PacEntity.getTileCoords(vec2.fromArray(this.position.xy));
+	}
+
+	public getNextTile(): MI {
+		const coords = this.getMapCoords().add(this.facing);
+		return tiles[coords.y][coords.x];
+	}
+
+	public update(deltaTime: number): boolean {
+		deltaTime /= 5;
+		const newPos = this.position.addValues(this.facing.x * deltaTime, this.facing.y * deltaTime, 0);
+		const newMapPos = PacEntity.getMapCoords(vec2.fromArray(newPos.xy));
+		const tile = tiles[newMapPos.y][newMapPos.x];
+		const canMove = this.canWalkOnTile(tile);
+		if (canMove) this.position = newPos;
+		else this.facing = PacEntity.randomDirection();
+
+		return super.update(deltaTime);
+	}
+
+	protected canWalkOnTile(tile: MI): boolean {
+		switch (tile) {
+			case MI._PS:
+			case MI._FS:
+			case MI.GSB:
+			case MI.GSP:
+			case MI.GSI:
+			case MI.GSC:
+
+			case MI._p_:
+			case MI._E_:
+
+			case MI._s_:
+			case MI.RU_:
+			case MI.RR_:
+			case MI.RUp:
+
+			case MI.GTB:
+			case MI.GTP:
+			case MI.GTI:
+			case MI.GTC:
+				return true;
+			default: return false;
+		}
+	}
+
+	/**
+	 * Get which tile this entity is currently on. Tile (0, 0) is the upper left most tile
+	 */
+	public static getMapCoords(position: vec2): vec2 {
+		return position.scale(1 / PIXELS_PER_TILE).floor();
+	}
+
+	/**
+	 * Get which pixel within a tile this entity is currently on. Pixel (0, 0) is the upper left most pixel within a tile
+	 */
+	public static getTileCoords(position: vec2): vec2 {
+		const x = position.x % PIXELS_PER_TILE;
+		const y = position.y % PIXELS_PER_TILE;
+		return new vec2(x, y);
+	}
+
+	public static randomDirection(): vec2 {
+		switch (Math.floor(Math.random() * 4)) {
+			case 0: return new vec2(0, +1);
+			case 1: return new vec2(0, -1);
+			case 2: return new vec2(+1, 0);
+			case 3: return new vec2(-1, 0);
+			default: console.log('Unknown direction!!!');
+		}
+	}
 }
 
 start();
