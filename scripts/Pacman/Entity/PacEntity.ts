39004e12d9fb453cebf382601d5d1ec1278f42b5
Alex Riedl <alexanderbriedl@gmail.com>
2017-09-25 22:54:10 -0500

    Starting to handle movement in a correct way

--- scripts/Pacman/Entity/PacEntity.ts
+++ scripts/Pacman/Entity/PacEntity.ts
@@ -7,11 +7,14 @@ import { vec2, vec3 } from 'Engine/Math';
 
 class PacEntity extends Entity {
 	private facing: PacEntity.Direction;
+	private desired: PacEntity.Direction;
 	private tilePosition: vec2;
 	private pixelPosition: vec2;
 
 	private randomizer: Random;
 	public map: Map;
+
+	private burn: number = 0;
 
 	// TODO: Map should not need to be passed into entity
 	public constructor(startTile: vec2, facingDirection: PacEntity.Direction, map: Map, randomizer: Random) {
@@ -19,21 +22,16 @@ class PacEntity extends Entity {
 
 		// TODO: Conversion between tiles and pixels in entity is strange...
 		this.tilePosition = startTile;
-		this.pixelPosition = new vec2(Map.PIXELS_PER_TILE - 0.5, Map.PIXELS_PER_TILE / 2);
+		this.pixelPosition = new vec2(Map.PIXELS_PER_TILE - 1, Map.PIXELS_PER_TILE / 2);
 		this.scale = new vec3(16, 16, 1);
 		this.facing = facingDirection;
+		this.desired = facingDirection;
 		this.map = map;
 		this.randomizer = randomizer;
 	}
 
 	public get position(): vec3 {
-		const p = this.tilePosition;
-		const ps = p.scale(Map.PIXELS_PER_TILE);
-		const psa = ps.add(this.pixelPosition);
-		const psa3 = psa.toVec3(0);
-		return psa3;
-
-		// return this.tilePosition.scale(Map.PIXELS_PER_TILE).add(this.pixelPosition).toVec3(0);
+		return this.tilePosition.scale(Map.PIXELS_PER_TILE).add(this.pixelPosition).toVec3(0);
 	}
 	public set position(value: vec3) {
 		console.log("Ignoring direct set of PacEntity's position value");
@@ -41,29 +39,89 @@ class PacEntity extends Entity {
 
 	// TODO: Perhaps limit max deltaTime to be a reasonable value to avoid some strange large frame problems
 	public update(deltaTime: number): boolean {
-
-		const t = true;
-		if (t) return t;
-
 		// TODO: Build a speed into the update based on deltaTime
 		// TODO: Does multiple updates per frame need to be supported?
 		// If based on the speed this entity wants to move more than one pixel, put this logic in a loop.
 		// Iterate over all update logic so no entity moves multiple pixels per update and clips through
 		// walls or causes other issues
-		const nextTile = PacEntity.move(this.tilePosition, this.facing); // this.tilePosition.add(this.facing);
-		const tileEnum = this.map.tiles[nextTile.y][nextTile.x];
-		const canMove = this.canWalkOnTile(tileEnum);
 
-		if (canMove) {
-			this.pixelPosition = PacEntity.move(this.pixelPosition, this.facing);
-			// NOTE: Ensure pixel position is valid
-			if (this.pixelPosition.x >= Map.PIXELS_PER_TILE || this.pixelPosition.x < 0 ||
-				this.pixelPosition.y >= Map.PIXELS_PER_TILE || this.pixelPosition.y < 0) {
-				this.tilePosition = nextTile;
-				this.pixelPosition = this.pixelPosition.mod(Map.PIXELS_PER_TILE);
+		this.burn += deltaTime;
+		const step = 100;
+		if (this.burn > step) {
+			this.burn -= step;
+		}
+		else {
+			return;
+		}
+
+		/*
+			- If DESIRED direction is same as FACING direction:
+				- Update pixel position based on FACING direction
+				- If new pixel is passed half way, check next tile
+					- If next tile is solid, deny movement
+			- IF DESIRED direction is OPPOSITE of FACING direction:
+				- Update pixel position based on DESIRED direction
+				- Always accept movement
+			- ELSE
+				- Update pixel position based on DESIRED direction and "towards center of tile" direction
+				- Always check next tile
+					- If next tile is solid, deny movement
+			- Re-orient pixel + tile positions
+		*/
+
+		let nextTile;
+		if (this.desired !== this.facing && !PacEntity.isOpposite(this.desired, this.facing)) {
+			nextTile = PacEntity.move(this.tilePosition, this.desired);
+			const tileEnum = this.map.tiles[nextTile.y][nextTile.x];
+			const canMove = this.canWalkOnTile(tileEnum);
+			if (canMove) {
+				let adjust;
+				if (this.desired === PacEntity.Direction.UP || this.desired === PacEntity.Direction.DOWN) {
+					adjust = new vec2(Math.sign(3 - this.pixelPosition.x), 0);
+				}
+				else {
+					adjust = new vec2(0, Math.sign(4 - this.pixelPosition.y));
+				}
+				const newPixelPos = PacEntity.move(this.pixelPosition, this.desired);
+				this.pixelPosition = newPixelPos.add(adjust);
+			}
+			else {
+				nextTile = undefined;
 			}
 		}
-		else this.facing = PacEntity.randomDirection(this.randomizer);
+
+		if (!nextTile) {
+			if (PacEntity.isOpposite(this.desired, this.facing)) {
+				this.facing = this.desired;
+				this.pixelPosition = PacEntity.move(this.pixelPosition, this.facing);
+				// NOTE: We just came from here, so it should be good
+				nextTile = PacEntity.move(this.tilePosition, this.facing);
+			}
+			else {
+				const tryTwo = PacEntity.move(this.pixelPosition, this.facing);
+				if ((this.facing === PacEntity.Direction.LEFT && tryTwo.x < 3) ||
+					(this.facing === PacEntity.Direction.RIGHT && tryTwo.x > 3) ||
+					(this.facing === PacEntity.Direction.UP && tryTwo.y > 4) ||
+					(this.facing === PacEntity.Direction.DOWN && tryTwo.y > 4)) {
+					nextTile = PacEntity.move(this.tilePosition, this.facing);
+					const tileEnum = this.map.tiles[nextTile.y][nextTile.x];
+					const canMove = this.canWalkOnTile(tileEnum);
+					if (canMove) this.pixelPosition = tryTwo;
+					else this.desired = PacEntity.randomDirection(this.randomizer);
+				}
+				else {
+					nextTile = this.tilePosition;
+					this.pixelPosition = tryTwo;
+				}
+			}
+		}
+
+		// NOTE: Ensure pixel position is valid
+		if (this.pixelPosition.x >= Map.PIXELS_PER_TILE || this.pixelPosition.x < 0 ||
+			this.pixelPosition.y >= Map.PIXELS_PER_TILE || this.pixelPosition.y < 0) {
+			this.tilePosition = nextTile;
+			this.pixelPosition = this.pixelPosition.cmod(Map.PIXELS_PER_TILE);
+		}
 
 		return super.update(deltaTime);
 	}
@@ -108,6 +166,27 @@ namespace PacEntity {
 		}
 	}
 
+	export function randomDirection(randomizer: Random): PacEntity.Direction {
+		const r = randomizer.nextRangeInt(0, 4);
+		switch (r) {
+			case 0: return PacEntity.Direction.RIGHT;
+			case 1: return PacEntity.Direction.LEFT;
+			case 2: return PacEntity.Direction.UP;
+			default: console.log('Unknown random direction selected...', r);
+			case 3: return PacEntity.Direction.DOWN;
+		}
+	}
+
+	export function isOpposite(d1: Direction, d2: Direction): boolean {
+		switch (d1) {
+			case Direction.RIGHT: return d2 === Direction.LEFT;
+			case Direction.LEFT: return d2 === Direction.RIGHT;
+			case Direction.UP: return d2 === Direction.DOWN;
+			case Direction.DOWN: return d2 === Direction.UP;
+			default: return false;
+		}
+	}
+
 	/**
 	 * Get which tile this entity is currently on. Tile (0, 0) is the upper left most tile
 	 */
@@ -122,15 +201,6 @@ namespace PacEntity {
 		const x = position.x % Map.PIXELS_PER_TILE;
 		const y = position.y % Map.PIXELS_PER_TILE;
 		return new vec2(x, y);
-	}
-
-	export function randomDirection(randomizer: Random): PacEntity.Direction {
-		switch (randomizer.nextRangeInt(0, 4)) {
-			case 0: return PacEntity.Direction.RIGHT;
-			case 1: return PacEntity.Direction.LEFT;
-			case 2: return PacEntity.Direction.UP;
-			case 3: return PacEntity.Direction.DOWN;
-		}
 	}
 }
 
